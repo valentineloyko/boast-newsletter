c312edb7d86f05234b4166a3dc5f846001644e25
Valentyn Loiko <valentynloiko@valentyns-air.home>
2024-07-17 10:49:39 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
+}
+
+export default App;
